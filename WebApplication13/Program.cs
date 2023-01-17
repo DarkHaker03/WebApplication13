982639b616995dd0ba7b8ebb65114bffe181c435
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2023-01-17 23:20:05 +0500

    fewdq

--- WebApplication13/Program.cs
+++ WebApplication13/Program.cs
@@ -11,7 +11,9 @@ builder.Services.AddDbContext<MySqlDbContext>(opts =>
 {
     var connectionString = "server=bd;port=3306;username=root;password=root;database=trialbd";
     opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+ 
 });
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins,
@@ -29,5 +31,10 @@ var app = builder.Build();
 
 app.MapControllers();
 app.UseRouting();
+
+
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetService<MySqlDbContext>();
+context.Database.Migrate();
 
 app.Run();
